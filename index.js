117ce6c5f6d43d4060dbcd86e0412efc759cdbd0
Cyril Gavala <cyril.gavala@dazn.com>
2020-12-06 13:17:05 +0100

    Changed to simple app

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
 const MongoClient = require('mongodb').MongoClient;
 const scrapy = require('node-scrapy');
 const fetch = require('node-fetch');
-const cron = require('node-cron');
 const uri = process.env.DB_URL;
 let data = [];
 
-cron.schedule('0 13 * * *', async function() {
-    console.log("Process started")
-    const url = 'https://korona.gov.sk/koronavirus-na-slovensku-v-cislach/';
-    const model = {
-        infos: [
-            '.app-pane-gray',
-            {
-                number: 'h2, h3',
-                label: 'p',
-            },
-        ]
-    };
-    fetch(url).then((res) => res.text()).catch(console.error)
-        .then((body) => {
-            data = scrapy.extract(body, model).infos
-            console.log("Data extracted")
-        }).catch(console.error)
+console.log("Process started")
+const url = 'https://korona.gov.sk/koronavirus-na-slovensku-v-cislach/';
+const model = {
+    infos: [
+        '.app-pane-gray',
+        {
+            number: 'h2, h3',
+            label: 'p',
+        },
+    ]
+};
+fetch(url).then((res) => res.text()).catch(console.error)
+    .then((body) => {
+        data = scrapy.extract(body, model).infos
+        console.log("Data extracted")
+    }).catch(console.error)
 
-    const clientResolved = await Promise.resolve(MongoClient.connect(uri, {useUnifiedTopology: true}).catch(err => console.error("Database unreachable:", err.name)));
-    if (clientResolved === undefined) return false;
-    const collection = clientResolved.db("covid-data").collection("daily-data");
-    let now = new Date();
-    now.setUTCHours(0, 0, 0, 0);
-    const dailyDataResolved = await Promise.resolve(collection.findOne({"date": now}, {}));
-    if (dailyDataResolved === null) {
-        let confirmed = parseInt(data[1].number.replace(/\s/g, "")),
-            recovered = parseInt(data[6].number.replace(/\s/g, "")),
-            deaths = parseInt(data[7].number.replace(/\s/g, ""));
-        let active = confirmed - recovered - deaths;
-        await collection.insertOne({
-            numberOfTests: parseInt(data[0].number.replace(/\s/g, "")),
-            confirmed: confirmed,
-            active: active,
-            recovered: recovered,
-            deaths: deaths,
-            date: now
-        }).catch(err => console.error("Entry was not saved", err));
-        console.log("Data saved to database");
-    }
-    await clientResolved.close();
-    console.log("Process finished");
-});
+const clientResolved = Promise.resolve(MongoClient.connect(uri, {useUnifiedTopology: true})
+    .catch(err => console.error("Database unreachable:", err.name)));
+if (clientResolved === undefined) return false;
+const collection = clientResolved.db("covid-data").collection("daily-data");
+let now = new Date();
+now.setUTCHours(0, 0, 0, 0);
+const dailyDataResolved = await Promise.resolve(collection.findOne({"date": now}, {}));
+if (dailyDataResolved === null) {
+    let confirmed = parseInt(data[1].number.replace(/\s/g, "")),
+        recovered = parseInt(data[6].number.replace(/\s/g, "")),
+        deaths = parseInt(data[7].number.replace(/\s/g, ""));
+    let active = confirmed - recovered - deaths;
+    collection.insertOne({
+        numberOfTests: parseInt(data[0].number.replace(/\s/g, "")),
+        confirmed: confirmed,
+        active: active,
+        recovered: recovered,
+        deaths: deaths,
+        date: now
+    }).catch(err => console.error("Entry was not saved", err));
+    console.log("Data saved to database");
+}
+clientResolved.close();
+console.log("Process finished");
