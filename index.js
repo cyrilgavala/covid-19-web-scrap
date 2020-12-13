3db40e2ffad7566705f89ccc0e705f394848ac52
Cyril Gavala <cyril.gavala@dazn.com>
2020-12-13 21:21:59 +0100

    adjusted workflow

--- index.js
+++ index.js
@@ -19,28 +19,27 @@ fetch(url).then((res) => res.text()).catch(console.error)
     .then((body) => {
         data = scrapy.extract(body, model).infos
         console.log("Data extracted")
+        MongoClient.connect(uri, {useUnifiedTopology: true})
+            .then(client => {
+                const collection = client.db("covid-data").collection("daily-data");
+                let now = new Date();
+                now.setUTCHours(0, 0, 0, 0);
+                let confirmed = parseInt(data[1].number.replace(/\s/g, "")),
+                    recovered = parseInt(data[6].number.replace(/\s/g, "")),
+                    deaths = parseInt(data[7].number.replace(/\s/g, "")),
+                    active = confirmed - recovered - deaths;
+                collection.findOneAndUpdate({"date": now}, {
+                    $set: {
+                        "numberOfTests": parseInt(data[0].number.replace(/\s/g, "")),
+                        "confirmed": confirmed,
+                        "active": active,
+                        "recovered": recovered,
+                        "deaths": deaths,
+                        "date": now
+                    }
+                }, {upsert: true}).then(() => {
+                    console.log("Data saved to database");
+                    client.close().then(r => console.log("Process finished", r)).catch(console.error);
+                }).catch(err => console.error("Entry was not saved", err));
+            }).catch(err => console.error("Database unreachable:", err))
     }).catch(console.error)
-
-MongoClient.connect(uri, {useUnifiedTopology: true})
-    .then(client => {
-        const collection = client.db("covid-data").collection("daily-data");
-        let now = new Date();
-        now.setUTCHours(0, 0, 0, 0);
-        let confirmed = parseInt(data[1].number.replace(/\s/g, "")),
-            recovered = parseInt(data[6].number.replace(/\s/g, "")),
-            deaths = parseInt(data[7].number.replace(/\s/g, "")),
-            active = confirmed - recovered - deaths;
-        collection.findOneAndUpdate({"date": now}, {
-            $set: {
-                "numberOfTests": parseInt(data[0].number.replace(/\s/g, "")),
-                "confirmed": confirmed,
-                "active": active,
-                "recovered": recovered,
-                "deaths": deaths,
-                "date": now
-            }
-        }, {upsert: true}).then(() => {
-            console.log("Data saved to database");
-            client.close().then(r => console.log("Process finished", r)).catch(console.error);
-        }).catch(err => console.error("Entry was not saved", err));
-    }).catch(err => console.error("Database unreachable:", err))
